19dc19a0c644aedc520417be00b3cdcdfd975ae6
JWP <jwpgdx@gmail.com>
2025-01-18 18:23:44 +0900

    Fix bug in button component

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
-import { Primitive, type PrimitiveProps } from 'radix-vue'
+import { Primitive, type PrimitiveProps as RadixPrimitiveProps } from 'radix-vue'
 import { type ButtonVariants, buttonVariants } from '.'
 
-interface Props extends PrimitiveProps {
+// PrimitiveProps 확장
+interface PrimitiveProps extends RadixPrimitiveProps {
+  as?: string
+  asChild?: boolean
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
 }
 
-const props = withDefaults(defineProps<Props>(), {
+// 기본값 설정
+const props = withDefaults(defineProps<PrimitiveProps>(), {
   as: 'button',
 })
 </script>
 
 <template>
   <Primitive
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :as="props.as"
+    :as-child="props.asChild"
+    :class="cn(buttonVariants({ variant: props.variant, size: props.size }), props.class)"
   >
     <slot />
   </Primitive>
